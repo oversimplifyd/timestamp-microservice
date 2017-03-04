2d1f8df65dbfab9cf2971fa34b4294a5f71dbd8a
Arotimi Busayo <thrustinv@gmail.com>
2017-03-04 22:18:30 +0000

    Initial Commit Timestamp Microservice

---
+++ timestamp.js
@@ -0,0 +1,60 @@
+'use strict'
+
+let moment = require('moment'),
+	express = require('express'),
+	app = express(),
+	unixTimeRegex = /\d+/,
+	stringTimeRegex = /^([a-zA-Z]{3,9})\s?\d{1,2},?\s?\d{4}$/g,
+	timeFormat = 'MMMM D, YYYY',
+	monthsArr = [	
+			'january',
+			'february',
+			'march',
+			'april',
+			'may',
+			'june',
+			'july',
+			'august',
+			'september',
+			'october',
+			'november',
+			'december' ],
+	input,
+	jsonOutput; 
+
+
+app.get('/:time_param', function(req, res){
+	
+	input = req.params.time_param;
+
+	if (stringTimeRegex.test(input)) {
+
+        let stringArray = input.split(' '),
+                month = monthsArr.indexOf(stringArray[0].toLowerCase()),
+                day = parseInt(stringArray[1]),
+                year = stringArray[2],
+                momentFromInput = moment([year, month, day]);
+
+                jsonOutput = {
+                    unix: momentFromInput.unix(),
+                    natural: input 
+                }    
+                
+    } else 
+    
+        if (unixTimeRegex.test(input))
+                jsonOutput = {
+                    unix: input,
+                    natural: moment.unix(Number(input)).format(timeFormat) 
+                }
+                
+        else 
+                jsonOutput = {
+				    unix: null,
+				    natural: null
+				}
+    
+    res.json(jsonOutput);	
+})
+
+app.listen(8080);
